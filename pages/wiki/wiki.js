5a18e870ed1531adf5f4855082da955811f54a44
Yareni Saavedra <yareni.saavedra@copilli.edu.mx>
2025-03-05 08:26:35 -0600

    Changes to wiki to add different texts

---
+++ pages/wiki/wiki.js
@@ -0,0 +1,16 @@
+// Emotion levels mapping (adjusted for levels 1-3)
+const emotionLevels = {
+    "Interest": ["", ""],
+    "Anticipation": "",
+    "Vigilance": ["Trepidation", "Fear", "Dread"],
+    "Boredom": ["Serenity", "Happiness", "Ecstasy"],
+    "Disgust": ["Anger", "Rage", "Fury"],
+    "Aversion": ["Melancholy", "Sadness", "Affliction"],
+    "7": ["Distraction", "Surprise", "Astonishment"],
+    "8": ["Acceptance", "Trust", "Admiration"],
+    "Anticipation": "",
+    "Anticipation": "",
+    "Anticipation": "",
+    "Anticipation": "",
+    "Anticipation": "",
+};
