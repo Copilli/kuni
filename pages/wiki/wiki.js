9582ef7d393c98107ebef70f5fc1e793723cf414
Yareni Saavedra <yareni.saavedra@copilli.edu.mx>
2025-03-05 09:06:56 -0600

    emotions
    I separated the emotions in diferent paraghraphs.

--- pages/wiki/wiki.js
+++ pages/wiki/wiki.js
@@ -1,16 +1,43 @@
 // Emotion levels mapping (adjusted for levels 1-3)
 const emotionLevels = {
     "Interest": ["", ""],
-    "Anticipation": "",
-    "Vigilance": ["Trepidation", "Fear", "Dread"],
-    "Boredom": ["Serenity", "Happiness", "Ecstasy"],
-    "Disgust": ["Anger", "Rage", "Fury"],
-    "Aversion": ["Melancholy", "Sadness", "Affliction"],
-    "7": ["Distraction", "Surprise", "Astonishment"],
-    "8": ["Acceptance", "Trust", "Admiration"],
-    "Anticipation": "",
-    "Anticipation": "",
-    "Anticipation": "",
-    "Anticipation": "",
-    "Anticipation": "",
+    "Anticipation": ["", ""],
+    "Vigilance": ["", ""],
+    "Boredom":["", ""],
+    "Disgust":["", ""],
+    "Aversion": ["", ""],
+    "Trepidation": ["", ""],
+    "Fear": ["", ""],
+    "Dread": ["", ""],
+    "Serenity": ["", ""],
+    "Happiness": ["", ""],
+    "Ecstasy": ["", ""],
+    "Anger": ["", ""],
+    "Rage": ["", ""],
+    "Fury": ["", ""],
+    "Melancholy": ["", ""],
+    "Sadness": ["", ""],
+    "Affliction": ["", ""],
+    "Distraction": ["", ""],
+    "Surprise": ["", ""],
+    "Astonishment": ["", ""],
+    "Acceptance": ["", ""],
+    "Trust": ["", ""],
+    "Admiration": ["", ""],
+
+
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
